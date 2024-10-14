d803c5936560d0b4ad402e12ee926ddaf2764f41
OmkarChobhe <157719603+OmkarChobhe@users.noreply.github.com>
2024-10-14 12:48:59 +0530

    Update Footer.jsx

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer-bg text-light p-4 '>
-        <h6 className='text-center'>All rights reserved &copy; Sanjay and Rudraksha</h6>
+        <h6 className='text-center'>All rights reserved &copy; Omkar , Sanjay and Rudraksha</h6>
     </div>
 
   )
